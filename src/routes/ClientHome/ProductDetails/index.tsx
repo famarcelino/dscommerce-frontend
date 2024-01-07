39076383835ae22ccfde74076c3de8688cc09906
Francisco de Assis Marcelino <famarcelino@gmail.com>
2024-01-07 16:16:18 -0300

    Catch request error

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,8 +2,7 @@ import './styles.css';
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { ProductDTO } from '../../../models/product';
 import * as productService from "../../../services/product-service";
@@ -12,13 +11,17 @@ export default function ProductDetails() {
 
     const params = useParams();
 
+    const navigate = useNavigate();
+
     const [product, setProduct] = useState<ProductDTO>();
 
     useEffect(() => {
         productService.findById(Number(params.productId))
             .then(response => {
-                console.log(response.data);
                 setProduct(response.data);
+            })
+            .catch(() => {
+                navigate("/");
             });
 
     }, []);
