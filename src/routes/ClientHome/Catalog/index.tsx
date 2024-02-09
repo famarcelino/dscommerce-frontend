85c2f94ae28813e1f1cba1f302d905a82b10a5e2
Francisco de Assis Marcelino <famarcelino@gmail.com>
2024-02-09 14:46:11 -0300

    hasAnyRoles

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -29,7 +29,7 @@ export default function Catalog() {
                 setProducts(products.concat(nextPage));
                 setIsLastPage(response.data.last);
             });
-    }, [queryParams]);
+    }, [products, queryParams]);
 
     function handleSearch(searchText: string) {
         setProducts([]);
