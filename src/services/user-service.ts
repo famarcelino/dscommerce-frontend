aa8292efeb28e4987635dd461c4a24886428f471
Francisco de Assis Marcelino <famarcelino@gmail.com>
2024-02-09 09:40:15 -0300

    requestBackend update withCredentials

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,11 +1,12 @@
 import { requestBackend } from "./requests";
-import * as authService from './auth-service';
+import { AxiosRequestConfig } from "axios";
 
 export function findMe() {
 
-    const headers = {
-        Authorization: "Bearer" + authService.getAccessToken()
+    const config : AxiosRequestConfig = {
+        url: "users/me",
+        withCredentials: true
     }
 
-    return requestBackend({ url: `/users/me`, headers });
+    return requestBackend(config);
 }
