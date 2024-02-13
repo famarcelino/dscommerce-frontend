58757ed2ead4645693c6e9d6a7529ee7da1ef74e
Francisco de Assis Marcelino <famarcelino@gmail.com>
2024-02-13 13:01:49 -0300

    forms update

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -2,6 +2,7 @@
 import './styles.css';
 import { useContext, useState } from 'react';
 import * as authService from '../../../services/auth-service';
+import * as forms from '../../../utils/forms';
 import { useNavigate } from 'react-router-dom';
 import { ContextToken } from '../../../utils/context-token';
 import FormInput from '../../../components/FormInput';
@@ -34,9 +35,7 @@ export default function Login() {
     })
 
     function handleInputChange(event: any) {
-        const value = event.target.value;
-        const name = event.target.name;
-        setFormData({ ...formData, [name]: { ...formData[name], value: value } });
+        setFormData(forms.update(formData, event.target.name, event.target.value));
     }
 
     function handleSubmit(event: any) {
